923c6fc8cb49a08a2d8b5955aeb57f2ad4d63e4b
Mauricio Mendoza <mauricio.mendoza.d@gmail.com>
2022-02-14 17:15:13 -0300

    integracion api marcar bicicletas en mapa

--- red-bicicletas/public/javascripts/map.js
+++ red-bicicletas/public/javascripts/map.js
@@ -4,6 +4,17 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-33.4476718,-70.6447573]).addTo(map);
-L.marker([-33.4450067,-70.646887]).addTo(map);
-L.marker([-33.4456681,-70.650457]).addTo(map);
+//L.marker([-33.4476718,-70.6447573]).addTo(map);
+//L.marker([-33.4450067,-70.646887]).addTo(map);
+//L.marker([-33.4456681,-70.650457]).addTo(map);
+
+$.ajax({
+	dataType: "json",
+	url: "api/bicicletas",
+	success: function(result) {
+		console.log(result);
+		result.bicicletas.forEach(function(bici) {
+			L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+		});
+	}
+})
